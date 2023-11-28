a10668dac8256ebe3fe466c5a61a49380f0f1b7f
lysenko_v <lysenko_v@itstep.academy>
2023-11-28 20:01:47 +0200

    1

---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+
+void writeStringsToFile(const std::string& filename);
+bool compareFiles(const std::string& filename1, const std::string& filename2);
+
+#endif // FUNCTIONS_H
