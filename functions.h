d17e4703b67c5410cb1441ce64e1f1e3a7ea947d
lysenko_v <lysenko_v@itstep.academy>
2023-11-28 19:48:43 +0200

    1

---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+
+void writeStringsToFile(const std::string& filename);
+void copyFileContent(const std::string& inputFilename, const std::string& outputFilename);
+
+#endif // FUNCTIONS_H
--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-  
+#include "functions.h"
+
+int main() {
+    const std::string inputFilename = "input.txt";
+    const std::string outputFilename = "output.txt";
+
+    writeStringsToFile(inputFilename);
+    copyFileContent(inputFilename, outputFilename);
+
+    return 0;
+}
