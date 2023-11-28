6331a5c82ad9f18c46453e5f19132371e6379ff8
lysenko_v <lysenko_v@itstep.academy>
2023-11-28 20:06:43 +0200

    1

---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+
+void writeStringToFile(const std::string& filename);
+void generateStatistics(const std::string& inputFilename, const std::string& outputFilename);
+
+#endif // FUNCTIONS_H
